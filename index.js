43d1dab7e7482641c6067b0f079182914d5627e7
LautaroSnchz <lautaro.snchz94@gmail.com>
2024-10-08 04:14:33 -0300

    Arreglos

---
+++ index.js
@@ -0,0 +1,19 @@
+
+    function mostrarToast() {
+        var toast = document.getElementById("toast");
+        toast.style.display = "block"; // Mostrar el toast
+        toast.style.opacity = "1"; // Asegúrate de que sea visible
+
+        // Ocultar el toast después de 3 segundos
+        setTimeout(function() {
+            toast.style.opacity = "0"; // Desvanecer el toast
+            setTimeout(function() {
+                toast.style.display = "none"; // Ocultar después del desvanecimiento
+            }, 500); // Tiempo de desvanecimiento
+        }, 6000); // Tiempo que el toast estará visible
+    }
+
+    // Mostrar el toast al cargar la página
+    window.onload = function() {
+        mostrarToast();
+    };
